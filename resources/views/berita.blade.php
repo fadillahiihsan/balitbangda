7a3eac924d94f552ad8b79d383b5be0c003a79bc
Ahmad Fadillah Ihsan <86653110+fadillahiihsan@users.noreply.github.com>
2022-11-14 07:09:27 +0000

    update

---
+++ resources/views/berita.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.template')
+
+@section('judul')
+Berita
+@endsection
+
+@section('main')
+<div class="container" data-aos="fade-up">
+  <div class="row gx-0">
+
+    <div class="col-lg-6 d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="200">
+      <div class="content">
+       INI HALAMAN BERITA
+      </div>
+    </div>
+
+    <div class="col-lg-6 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="200">
+      <img src="https://balitbangda.kalselprov.go.id/wp-content/uploads/photo-gallery/IMG_3719.JPG" class="img-fluid" alt="">
+    </div>
+
+  </div>
+</div>
+@endsection
