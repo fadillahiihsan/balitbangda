1a7e6354002cb67730a90fbe5477f70568d06156
Ahmad Fadillah Ihsan <86653110+fadillahiihsan@users.noreply.github.com>
2022-11-14 07:17:49 +0000

    update

--- resources/views/berita.blade.php
+++ resources/views/berita.blade.php
@@ -7,7 +7,7 @@ Berita
 @section('main')
 <div class="container" data-aos="fade-up">
   <div class="row gx-0">
-
+    
     <div class="col-lg-6 d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="200">
       <div class="content">
        INI HALAMAN BERITA tes
